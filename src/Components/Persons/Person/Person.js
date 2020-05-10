a0af52f62ef93014c298d1bca736de46a3a47d9b
ramsian <sriram.vallabhaneni51@gmail.com>
2020-05-10 19:29:34 +0530

    Using HOC to return adjacent JSX elements

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Aux from '../../../hoc/Auxillary';
 
 import classes from './Person.css';
 
@@ -6,13 +7,13 @@ class Person extends Component {
   render() {
     console.log('{Person.js} rendering...')
     return(
-      <div className={classes.Person}>
+      <Aux>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </div>
+      </Aux>
     )
   }
 };
