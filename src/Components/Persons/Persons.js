f385ffb7341797e1325851fd991bd63702c9ce2e
ramsian <sriram.vallabhaneni51@gmail.com>
2020-05-10 15:17:16 +0530

    Converting functional stateless components to class components

--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const Persons = (props) => {
-    return(
-        props.persons.map( (person, index) => {
-            return(
-                <Person click={() => props.clicked(index)} name={person.name} age={person.age} key={person.id} changed={event => props.changed(event, person.id)}></Person>
-            )
-        })
-    );
+class Persons extends Component {
+    render() {
+        return(
+            this.props.persons.map( (person, index) => {
+                return(
+                    <Person click={() => this.props.clicked(index)} name={person.name} age={person.age} key={person.id} changed={event => this.props.changed(event, person.id)}></Person>
+                )
+            })
+        )
+    }
 };
 
 export default Persons;
